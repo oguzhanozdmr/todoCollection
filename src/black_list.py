733bab9d3289dd684d46b3a19eb9f9bb2ee7261e
Oğuzhan Özdemir <ademoguzhanozdmr@gmail.com>
2023-01-04 16:22:50 +0300

    user logout blacklist

---
+++ src/black_list.py
@@ -0,0 +1,7 @@
+'''safe user logout like redis storage'''
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+BLACK_LIST = set()
+
+#TODO: Performans icin token redisten cikana kadar expire olacak sekilde saklanabilir
