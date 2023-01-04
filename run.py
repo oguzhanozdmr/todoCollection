b31615f88da3f1d5976ac7ccafff2193f419e538
Oğuzhan Özdemir <ademoguzhanozdmr@gmail.com>
2023-01-04 16:24:10 +0300

    server run file

--- run.py
+++ run.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
+
+from os import environ
+from src import create_app
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+def main():
+    app = create_app()
+    app.run(debug=True, host='0.0.0.0', port=8080)
+    
+if __name__ == '__main__':
+    main()
