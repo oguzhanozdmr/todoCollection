75f6303e982db81828e9f0e99658110ad26e1af0
Oğuzhan Özdemir <ademoguzhanozdmr@gmail.com>
2023-01-04 16:23:35 +0300

    added db connection info

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,9 @@
 #!/usr/bin/env python
 
 from os import environ
-from flask import Flask
-
+from flask import Flask, jsonify
+from src.utils import error_as_json
+from src.extensions import db
 
 __all__ = ['create_app']
 
@@ -14,6 +15,19 @@ __status__ = environ.get('deployment_status')
 def create_app() -> Flask: 
     app = Flask(__name__)
     app.secret_key = environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'postgresql://skydata:pvFQMzdN*2Ky@localhost:5432/skyadatabase')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    
+    db.init_app(app)
+    register_blueprints(app)
+    
+    @app.errorhandler(400)
+    def bad_request(e):
+        return error_as_json(msg="invalid request"), 400
     
     return app
+
+def register_blueprints(app: Flask):
+    from src.controllers.auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
